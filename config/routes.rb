e5f7709468e7dfd240d76a29918f90b2a30d53a0
charcodes24 <charcodes24@gmail.com>
2021-08-18 10:12:31 -0700

    commit tree c5d9825ff3976649534b24fd29628f04971f9d8e

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :movies
+  resources :movies, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  def index
+    movies = Movie.all 
+    render json: movies 
+  end
 end
